8407e512a35fad24c5f870ac858ff01d3216b4d0
Keitaro Oguri <ogukei256@gmail.com>
2024-02-17 06:12:26 +0900

    Add log

--- rcon_cli/src/main.rs
+++ rcon_cli/src/main.rs
@@ -4,20 +4,24 @@ use std::env;
 
 use anyhow::{bail, Result};
 use rcon::{client::RconClient, Packet, PacketType};
+use log::trace;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // connect
+    trace!("connecting");
     let endpoint = env::var("RCON_ENDPOINT").expect("RCON_ENDPOINT is required");
     let client = RconClient::connect(&endpoint).await?;
-    println!("connected");
+    trace!("connected");
     // auth
     const AUTH_PACKET_ID: i32 = 0;
+    
     let password = env::var("RCON_PASSWORD").expect("RCON_PASSWORD is required");
     let auth_request = Packet::new(AUTH_PACKET_ID, PacketType::AUTH, password.into())?;
+    trace!("sending auth request");
     client.write_packet(auth_request).await?;
     // await next auth response
-    println!("awaiting auth reponse");
+    trace!("awaiting auth reponse");
     let auth_response = loop {
         let packet = client.read_packet().await?;
         if packet.r#type() == PacketType::AUTH_RESPONSE {
@@ -26,7 +30,7 @@ async fn main() -> Result<()> {
     };
     // check auth result
     if auth_response.id() == AUTH_PACKET_ID {
-        println!("authentication successful");
+        trace!("authentication successful");
     } else {
         bail!("authentication failure");
     }
@@ -34,9 +38,10 @@ async fn main() -> Result<()> {
     const COMMAND_PACKET_ID: i32 = 1;
     let command = env::var("RCON_COMMAND").expect("RCON_COMMAND is required");
     let command_request = Packet::new(COMMAND_PACKET_ID, PacketType::EXEC_COMMAND, command)?;
+    trace!("sending command request");
     client.write_packet(command_request).await?;
     // awaiting command response
-    println!("awaiting command reponse");
+    trace!("awaiting command reponse");
     let response_value = loop {
         let packet = client.read_packet().await?;
         if packet.id() == COMMAND_PACKET_ID && packet.r#type() == PacketType::RESPONSE_VALUE {
@@ -44,6 +49,7 @@ async fn main() -> Result<()> {
         }
     };
     let response_body = response_value.body()?;
-    println!("command result: {}", response_body);
+    trace!("command response acquired");
+    println!("{}", response_body);
     Ok(())
 }
